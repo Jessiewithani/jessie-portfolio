6fd5a965dd991431143c083a9dafafe5633bd890
Jessie Le-Ho <lehoj@oregonstate.edu>
2023-03-16 21:30:30 -0600

    Create and style header and create projects container

--- src/Intro.jsx
+++
@@ -1,7 +0,0 @@
-export default function Intro() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
-}
---
+++ src/LandingPage/ProjectsContainer.jsx
@@ -0,0 +1,9 @@
+import '../LandingPage/styles/projects-container.scss';
+
+export default function ProjectsContainer() {
+  return (
+    <div className="projects-container">
+      <h2>projects go here as buttons</h2>
+    </div>
+  )
+}
