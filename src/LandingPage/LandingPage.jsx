715d306e58962e65fe8340aacf0de2e420d685d3
Jessie Le-Ho <lehoj@oregonstate.edu>
2023-03-17 19:15:36 -0600

    Style and add images to project buttons

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import PortfolioHeader from '../LandingPage/PortfolioHeader';
 import ProjectsContainer from '../LandingPage/ProjectsContainer';
 import './styles/landing-page.scss';
 
-export default function Intro() {
+export default function LandingPage() {
   return (
     <div className="intro-wrapper">
       <PortfolioHeader />
