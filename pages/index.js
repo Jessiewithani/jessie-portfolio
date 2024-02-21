579cfa64a37b9af1a7cd4cff388a0347f59b3377
Jessie Le-Ho <lehoj@oregonstate.edu>
2024-02-21 00:20:30 -0700

    WIP convert SCSS into tailwind

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import { LandingPage } from './LandingPage/LandingPage';
+
+export default Home = () => {
+  return <LandingPage />
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,ts, js}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
