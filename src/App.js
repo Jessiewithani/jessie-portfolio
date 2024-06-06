a79437af4ea9dee99697c9ef6dd619527c4237d0
jessie <jessie@jessies-MacBook-Pro.local>
2024-06-06 14:54:54 -0600

    Configure tsc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import LandingPage from './landingPage/LandingPage'
+import LandingPage from './LandingPage/LandingPage'
 import About from './About/About';
 import Skills from './Skills/Skills';
 import './globals.css';
